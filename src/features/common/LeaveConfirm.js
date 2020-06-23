065a2fe89cbc32d970849d7595c242acc0261600
ever <892938772@qq.com>
2020-06-23 17:25:46 +0800

    将不涉及实例状态的方法改为静态方法

--- src/features/common/LeaveConfirm.js
+++ src/features/common/LeaveConfirm.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import * as actions from './redux/actions';
 
-export class LeaveConfirm extends Component {
-  static propTypes = {
-    common: PropTypes.object.isRequired,
-    actions: PropTypes.object.isRequired,
-  };
-
-  goBack(props) {
-    props.history.goBack();
-  }
-
-  goHome(props) {
-    props.history.push('/blog');
-  }
+export default class LeaveConfirm extends Component {
 
   render() {
+    const goBack = (props) => props.history.goBack();
+    const goHome = (props) => props.history.push('/blog');
     return (
       <div className="common-leave-confirm">
         <div className="common-leave-confirm-tip">
@@ -27,12 +13,12 @@ export class LeaveConfirm extends Component {
             <h3>即将离开whyyy的博客</h3>
             <p>常肥来看看~~</p>
             <p>{this.props.location.state}</p>
-            <a onClick={() => this.goBack(this.props)} href={this.props.location.state} target="_blank"
+            <a onClick={() => goBack(this.props)} href={this.props.location.state} target="_blank"
                rel="noopener norefferrer">
               继续访问</a>
-            <a onClick={() => this.goBack(this.props)}>
+            <a onClick={() => goBack(this.props)}>
               返回上一层</a>
-            <a onClick={() => this.goHome(this.props)}>
+            <a onClick={() => goHome(this.props)}>
               返回主页</a>
           </div>
         </div>
@@ -40,22 +26,3 @@ export class LeaveConfirm extends Component {
     );
   }
 }
-
-/* istanbul ignore next */
-function mapStateToProps(state) {
-  return {
-    common: state.common,
-  };
-}
-
-/* istanbul ignore next */
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators({ ...actions }, dispatch),
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(LeaveConfirm);
--- src/features/common/PageNotFound.js
+++ src/features/common/PageNotFound.js
@@ -1,18 +1,11 @@
 import React, { PureComponent } from 'react';
-// import lost from '../../images/lost.jpeg';
 import lost from '../../images/cute-girl.png';
 
 export default class PageNotFound extends PureComponent {
 
-  goBack(props) {
-    props.history.goBack();
-  }
-
-  goHome(props) {
-    props.history.push('/blog');
-  }
-
   render() {
+    const goBack = (props) => props.history.goBack();
+    const goHome = (props) => props.history.push('/blog');
     return (
       <div className="common-page-not-found">
         <div className="common-page-not-found-tip">
@@ -20,13 +13,12 @@ export default class PageNotFound extends PureComponent {
           <div>
             <h2>不是我搬家了，就是你迷路啦</h2>
             <div className="common-page-not-found-back">
-              <a onClick={() => this.goBack(this.props)}>
+              <a onClick={() => goBack(this.props)}>
                 返回上一层</a>
               <li><i className="fa fa-level-down"> </i></li>
             </div>
-
             <div className="common-page-not-found-home">
-              <a onClick={() => this.goHome(this.props)}>
+              <a onClick={() => goHome(this.props)}>
                 返回主页</a>
               <li><i className="fa fa-home"> </i></li>
             </div>
