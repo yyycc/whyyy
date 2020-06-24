d290e983be7e79f729118c2a52e83c373ec308ca
ever <892938772@qq.com>
2020-06-24 11:12:39 +0800

    引入方式问题，导致CarouselIndex组件获取不到状态

--- src/features/blog/Body/Index/HomePage.js
+++ src/features/blog/Body/Index/HomePage.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../../redux/actions';
-import { CarouselIndex } from './CarouselIndex/CarouselIndex';
+import CarouselIndex from './CarouselIndex/CarouselIndex';
 import { Post } from './ListsIndex/Post';
 import { Pagination } from 'antd';
 
