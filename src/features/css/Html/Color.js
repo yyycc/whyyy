aad349c403e68cc22220a82811dea35c1ad63e98
ever <892938772@qq.com>
2020-06-25 16:52:53 +0800

    去掉redux

--- src/features/css/Html/Color.js
+++ src/features/css/Html/Color.js
@@ -1,73 +1,121 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { changeColor } from '../redux/actions';
 
-export class Color extends Component {
-  static propTypes = {
-    actions: PropTypes.object.isRequired,
-  };
+const js = 'rgb2hex(sRGB) {\n' +
+  '  let left = sRGB.replace(\'rgb(\', \'\').replace(\')\', \'\').replace(\' \', \'\');\n' +
+  '  let arr = left.split(\',\');\n' +
+  '  if (arr.length !== 3 && arr.length !== 4) {\n' +
+  '    return \'\';\n' +
+  '  }\n' +
+  '  let sexes = [];\n' +
+  '  for (let i = 0; i < arr.length; i++) {\n' +
+  '    if (arr[i] >= 0 && arr[i] < 256) {\n' +
+  '      let sex = Number(arr[i]).toString(16);\n' +
+  '      if (Number(arr[i]) < 16) {\n' +
+  '        sex = \'0\' + sex;\n' +
+  '      }\n' +
+  '      sexes.push(sex);\n' +
+  '    } else {\n' +
+  '      return \'\';\n' +
+  '    }\n' +
+  '  }\n' +
+  '  return \'#\' + sexes.join(\'\');\n' +
+  '}\n' +
+  '\n' +
+  'hex2rgb(sRGB) {\n' +
+  '  sRGB = sRGB.replace(\'#\', \'\');\n' +
+  '  let arrOrigin = Array.from(sRGB);\n' +
+  '  let arr = [];\n' +
+  '  if (arrOrigin.length === 3) {\n' +
+  '    for (let i = 0; i < 3; i++) {\n' +
+  '      arr.push(arrOrigin[i] + arrOrigin[i]);\n' +
+  '    }\n' +
+  '  } else if (arrOrigin.length === 6) {\n' +
+  '    for (let i = 0; i < 6; i++) {\n' +
+  '      arr.push(arrOrigin[i] + arrOrigin[++i]);\n' +
+  '    }\n' +
+  '  } else {\n' +
+  '    return \'\';\n' +
+  '  }\n' +
+  '  let sexes = \'\';\n' +
+  '  for (let i = 0; i < arr.length; i++) {\n' +
+  '    if (arr[i] >= \'00\' && arr[i] <= \'ff\') {\n' +
+  '      let sex = Number(\'0x\' + arr[i]).toString(10) + \',\';\n' +
+  '      sexes += sex;\n' +
+  '    } else {\n' +
+  '      return \'\';\n' +
+  '    }\n' +
+  '  }\n' +
+  '  return \'rgb(\' + sexes.slice(0, sexes.length - 1) + \')\';\n' +
+  '}';
 
-  rgb2hex(sRGB) {
-    let left = sRGB.replace('rgb(', '').replace(')', '').replace(' ', '');
-    let arr = left.split(',');
-    if (arr.length !== 3 && arr.length !== 4) {
-      return '';
-    }
-    let sexes = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] >= 0 && arr[i] < 256) {
-        let sex = Number(arr[i]).toString(16);
-        if (Number(arr[i]) < 16) {
-          sex = '0' + sex;
-        }
-        sexes.push(sex);
-      } else {
-        return '';
-      }
-    }
-    return '#' + sexes.join('');
+const rgb2hex = sRGB => {
+  let left = sRGB.replace('rgb(', '').replace(')', '').replace(' ', '');
+  let arr = left.split(',');
+  if (arr.length !== 3 && arr.length !== 4) {
+    return '';
   }
-
-  hex2rgb(sRGB) {
-    sRGB = sRGB.replace('#', '');
-    let arrOrigin = Array.from(sRGB);
-    let arr = [];
-    if (arrOrigin.length === 3) {
-      for (let i = 0; i < 3; i++) {
-        arr.push(arrOrigin[i] + arrOrigin[i]);
+  let sexes = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0 && arr[i] < 256) {
+      let sex = Number(arr[i]).toString(16);
+      if (Number(arr[i]) < 16) {
+        sex = '0' + sex;
       }
-    } else if (arrOrigin.length === 6) {
-      for (let i = 0; i < 6; i++) {
-        arr.push(arrOrigin[i] + arrOrigin[++i]);
-      }
+      sexes.push(sex);
     } else {
       return '';
     }
-    let sexes = '';
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] >= '00' && arr[i] <= 'ff') {
-        let sex = Number('0x' + arr[i]).toString(10) + ',';
-        sexes += sex;
-      } else {
-        return '';
-      }
+  }
+  return '#' + sexes.join('');
+};
+
+const hex2rgb = sRGB => {
+  sRGB = sRGB.replace('#', '');
+  let arrOrigin = Array.from(sRGB);
+  let arr = [];
+  if (arrOrigin.length === 3) {
+    for (let i = 0; i < 3; i++) {
+      arr.push(arrOrigin[i] + arrOrigin[i]);
     }
-    return 'rgb(' + sexes.slice(0, sexes.length - 1) + ')';
+  } else if (arrOrigin.length === 6) {
+    for (let i = 0; i < 6; i++) {
+      arr.push(arrOrigin[i] + arrOrigin[++i]);
+    }
+  } else {
+    return '';
+  }
+  let sexes = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= '00' && arr[i] <= 'ff') {
+      let sex = Number('0x' + arr[i]).toString(10) + ',';
+      sexes += sex;
+    } else {
+      return '';
+    }
+  }
+  return 'rgb(' + sexes.slice(0, sexes.length - 1) + ')';
+};
+
+const calc = (id, value) => {
+  if (id === 'rgb') {
+    document.getElementById('hex').value = rgb2hex(value);
   }
 
-  calc(id, value) {
-    if (id === 'rgb') {
-      document.getElementById('hex').value = this.rgb2hex(value);
-    }
+  if (id === 'hex') {
+    document.getElementById('rgb').value = hex2rgb(value);
+  }
+};
 
-    if (id === 'hex') {
-      document.getElementById('rgb').value = this.hex2rgb(value);
-    }
+export default class Color extends Component {
+  constructor() {
+    super();
+    this.state = {
+      color: 'none',
+      js: js,
+    };
   }
 
-  onBlur(e, changeColor) {
+  onBlur(e) {
     let value = e.target.value;
     if (value.indexOf(',') >= 0 && value.indexOf('rgb') < 0) {
       value = 'rgb(' + value + ')';
@@ -75,11 +123,14 @@ export class Color extends Component {
     if (value.indexOf(',') < 0 && value.indexOf('#') < 0) {
       value = '#' + value;
     }
-    changeColor(value);
-    this.calc(e.target.id, value);
+    this.setState({
+      ...this.state,
+      color: value,
+    });
+    calc(e.target.id, value);
   }
 
-  onEnter(event, changeColor) {
+  onEnter(event) {
     let code;
     if (event.key !== undefined) {
       code = event.key;
@@ -96,15 +147,15 @@ export class Color extends Component {
       if (value.indexOf(',') < 0 && value.indexOf('#') < 0) {
         value = '#' + value;
       }
-      changeColor(value);
-      this.calc(event.target.id, value);
+      this.setState({
+        ...this.state,
+        color: value,
+      });
+      calc(event.target.id, value);
     }
   }
 
   render() {
-    let color = this.props.color;
-    let js = this.props.js;
-    const { changeColor } = this.props.actions;
     return (
       <div className="css-color">
 
@@ -112,8 +163,8 @@ export class Color extends Component {
           <h3>RGB</h3>
           <div className="css-layout-body-rgb-content">
             <p>输入rgb格式</p>
-            <input placeholder='rgb(255,255,255)' id='rgb' onBlur={(e) => this.onBlur(e, changeColor)}
-                   onKeyPress={(e) => this.onEnter(e, changeColor)}/>
+            <input placeholder='rgb(255,255,255)' id='rgb' onBlur={(e) => this.onBlur(e)}
+                   onKeyPress={(e) => this.onEnter(e)}/>
             <dl>
               <dt>支持格式</dt>
               <dd>
@@ -134,16 +185,16 @@ export class Color extends Component {
         <div className="css-layout-body-show">
           <h3>预览</h3>
           <div className="css-layout-body-show-content"
-               style={{ background: color !== 'none' ? 'none' : '#fff url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEX///8AAABVwtN+AAAAAnRSTlMzMz0oyR4AAAARSURBVAgdY/jPwIAVYRf9DwB+vw/xbMOy9QAAAABJRU5ErkJggg==) 50%' }}>
-            <div style={{ backgroundColor: `${color}` }}/>
+               style={{ background: this.state.color !== 'none' ? 'none' : '#fff url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEX///8AAABVwtN+AAAAAnRSTlMzMz0oyR4AAAARSURBVAgdY/jPwIAVYRf9DwB+vw/xbMOy9QAAAABJRU5ErkJggg==) 50%' }}>
+            <div style={{ backgroundColor: `${this.state.color}` }}/>
           </div>
         </div>
         <div className="css-layout-body-hex">
           <h3>HEX</h3>
           <div className="css-layout-body-hex-content">
             <p>输入hex格式</p>
-            <input placeholder='#ffffff' id='hex' onBlur={(e) => this.onBlur(e, changeColor)}
-                   onKeyPress={(e) => this.onEnter(e, changeColor)}/>
+            <input placeholder='#ffffff' id='hex' onBlur={(e) => this.onBlur(e)}
+                   onKeyPress={(e) => this.onEnter(e)}/>
             <dl>
               <dt>支持格式</dt>
               <dd>
@@ -165,7 +216,7 @@ export class Color extends Component {
           <h3>方法</h3>
           <pre>
             <code className="javaScript">
-                {js}
+                {this.state.js}
             </code>
           </pre>
         </div>
@@ -173,24 +224,3 @@ export class Color extends Component {
     );
   }
 }
-
-/* istanbul ignore next */
-function mapStateToProps(state) {
-  return {
-    color: state.css.color,
-    js: state.css.js,
-  };
-}
-
-/* istanbul ignore next */
-function mapDispatchToProps(dispatch) {
-  return {
-    // actions: bindActionCreators({changeColor} , dispatch),
-    actions: { changeColor: bindActionCreators(changeColor, dispatch) },
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Color);
